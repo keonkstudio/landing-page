0997cb7c5f0891aa451143c247dbd09b034ac26e
bagoesrex <bagoesrex@gmail.com>
2026-03-04 15:05:31 +0700

    chore(ui): setup shadcn component and main icon

---
+++ components/layout/max-width-wrapper.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+interface MaxWidthWrapperProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export default function MaxWidthWrapper({ children, className }: MaxWidthWrapperProps) {
+  return <div className={cn(`mx-auto w-full max-w-6xl px-3.5`, className)}>{children}</div>;
+}
